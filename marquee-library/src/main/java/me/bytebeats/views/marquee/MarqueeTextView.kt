02a08d6c75925468eb52908a0831253f1e359e6f
panchen <panchen@tigerbrokers.com>
2021-08-19 15:46:43 +0800

    Initial commit

---
+++ marquee-library/src/main/java/me/bytebeats/views/marquee/MarqueeTextView.kt
@@ -0,0 +1,9 @@
+package me.bytebeats.views.marquee
+
+/**
+ * Created by bytebeats on 2021/8/19 : 15:45
+ * E-mail: happychinapc@gmail.com
+ * Quote: Peasant. Educated. Worker
+ */
+class MarqueeTextView {
+}
